054f4a3b0d53090e3d68c02e411e6b1130257fca
Shawn <lishawn0614@gmail.com>
2025-01-05 12:51:47 +0800

    加入專案檔案。

---
+++ 0105.大樂透/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _0105.大樂透
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            textBox1.Location = new Point(119, 50);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(830, 198);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(119, 276);
+            button1.Name = "button1";
+            button1.Size = new Size(235, 125);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1011, 504);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ 0105.大樂透/Form1.cs
@@ -0,0 +1,44 @@
+namespace _0105.大樂透
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            int[] Numbers = new int[100];
+            int a;
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = i + 1;
+            }
+            Random crandom = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                a = crandom.Next(1, Numbers.Length);
+                if (a < 10)
+                {
+                    textBox1.Text += 0;
+                }
+                textBox1.Text += a.ToString() + Environment.NewLine;
+                int[] NewNumbers = new int[Numbers.Length];
+                for(int j = 0; j < Numbers.Length; j++)
+                {
+                    if (j == a)
+                    {
+                        continue;
+                    }
+                    NewNumbers[j] = Numbers[j];
+                }
+                for (int j = 0; j < Numbers.Length; j++)
+                {
+                    Numbers[j] = NewNumbers[j];
+                }
+            }
+        }
+    }
+}
